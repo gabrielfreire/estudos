2affcdd3cba0600a4470360244ce16a25f29fbd2
Gabriel Freire <gabrielfreiredev@gmail.com>
2016-09-06 15:58:04 -0300

    refactoring...

--- src/com/gabriel/estudos/Excecoes.java
+++ src/com/gabriel/estudos/Excecoes.java
@@ -6,15 +6,19 @@ public class Excecoes {
 
 	public static void main(String[] args) {
 
-
+		//any variable defined inside try{} block isn't available in catch/finally blocks
+		//the variable must be defined outside the try{} block
 		int x = getInt();
 		int y = getInt();
+		int result= 999;
 		//treating an Unchecked exception
 		try{
-			int result = x / y;
+			result = x / y;
 			System.out.println(result);
 		}catch(ArithmeticException e){
 			System.out.println("Este número é indivisível por 0");
+			System.out.println("ERRO: " + e.getMessage());
+			System.out.println(result);
 		}
 		
 		
